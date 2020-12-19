990d6feff1c05ec1830f96febde6dcfe6219ee86
Martin studna <martin.studna2@gmail.com>
2020-12-20 00:15:23 +0100

    feat: progress

---
+++ webpack.config.ts
@@ -0,0 +1,42 @@
+import path from "path";
+import webpack from "webpack";
+import HtmlWebpackPlugin  from 'html-webpack-plugin'
+
+const config: webpack.Configuration = {
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index_bundle.js",
+  },
+  performance: {
+    hints: false
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: { presets: [
+            "@babel/preset-env",
+            "@babel/preset-react",
+            "@babel/preset-typescript",
+          ] },
+        },
+      },
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html'),
+    }),
+  ],
+};
+
+
+export default config;
